3741715b682e76d01972590add4d01601cbccd01
siddesh <siddesh@yahoo-inc.com>
2015-04-01 14:28:25 -0700

    Basic working model of tree view

--- testPage.js
+++ testPage.js
@@ -1,47 +1,41 @@
-//
-//var treeNode, dataObj, treeNodeObj;
-//
-//dataObj = {
-//    "folder-1": {
-//        "label": "first-folder",
-//        "type": "treeView",
-//        "children": [
-//            {
-//                label: "level1-1",
-//                type: "treeView",
-//                children: [
-//                    {label: "level2-1"},
-//                    {label: "level2-2"}
-//                ]
-//            },
-//            {
-//                label : "level1-2"
-//            }
-//        ]
-//    },
-//    "folder-2": {
-//        "label": "second-folder",
-//        "type": "treeView",
-//        "children": [
-//            {
-//                label: "level1-1",
-//                type: "treeView",
-//                children: [
-//                    {label: "level2-1"},
-//                    {label: "level2-2"}
-//                ]
-//            },
-//            {
-//                label : "level1-2"
-//            }
-//        ]
-//    }
-//};
-//
-//console.log("something is coming");
-//
-//treeNode = new tree($('.treeContainer .tree', dataObj));
-//
-//treeNodeObj = treeNode.getTree();
 
-console.log("hello");
+var treeNode, dataObj, treeNodeObj;
+
+dataObj = {
+    "folder-1": {
+        "label": "first-folder",
+        "type": "treeView",
+        "children": [
+            {
+                label: "A",
+                type: "treeView",
+                children: [
+                    {label: "C"}
+                ]
+            },
+            {
+                label : "B"
+            }
+        ]
+    },
+    "folder-2": {
+        "label": "second-folder",
+        "type": "treeView",
+        "children": [
+            {
+                label: "1",
+                type: "treeView",
+                children: [
+                    {label: "3"}
+                ]
+            },
+            {
+                label : "2"
+            }
+        ]
+    }
+};
+
+treeNode = new tree($('.treeContainer .tree'), dataObj);
+treeNodeObj = treeNode.getTree();
+
