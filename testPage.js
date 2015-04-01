7c2c1600921360b401573ad33039b28fef988e81
siddesh <siddesh@yahoo-inc.com>
2015-04-01 10:59:43 -0700

    Initial commit of tree view

---
+++ testPage.js
@@ -0,0 +1,47 @@
+//
+//var treeNode, dataObj, treeNodeObj;
+//
+//dataObj = {
+//    "folder-1": {
+//        "label": "first-folder",
+//        "type": "treeView",
+//        "children": [
+//            {
+//                label: "level1-1",
+//                type: "treeView",
+//                children: [
+//                    {label: "level2-1"},
+//                    {label: "level2-2"}
+//                ]
+//            },
+//            {
+//                label : "level1-2"
+//            }
+//        ]
+//    },
+//    "folder-2": {
+//        "label": "second-folder",
+//        "type": "treeView",
+//        "children": [
+//            {
+//                label: "level1-1",
+//                type: "treeView",
+//                children: [
+//                    {label: "level2-1"},
+//                    {label: "level2-2"}
+//                ]
+//            },
+//            {
+//                label : "level1-2"
+//            }
+//        ]
+//    }
+//};
+//
+//console.log("something is coming");
+//
+//treeNode = new tree($('.treeContainer .tree', dataObj));
+//
+//treeNodeObj = treeNode.getTree();
+
+console.log("hello");
